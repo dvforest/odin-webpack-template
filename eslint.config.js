e568b527df43ef6f0e0b8b8ad59736e04d251c44
dvforest <dvforest@hotmail.com>
2025-08-15 19:12:24 -0400

    Install prettier and eslint by default in this template.

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default defineConfig([
+    {
+        files: ['**/*.js'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'no-unused-vars': 'off',
+        },
+    },
+]);
